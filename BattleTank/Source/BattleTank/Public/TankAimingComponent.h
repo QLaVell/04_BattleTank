edbcc4ce35c58a43c7baf2cfd8c5dd7ec9b9cd7e
Quintin Oliver <quint.oliver@gmail.com>
2018-06-26 08:00:42 -0400

    Added blueprint node to set barrel for aim start location. Video 139: BlueprintCallable()

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -15,6 +15,8 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
 	void AimAt(FVector WorldSpaceAim);
 
 protected:
@@ -25,6 +27,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	UStaticMeshComponent* Barrel = nullptr;
 	
 };
