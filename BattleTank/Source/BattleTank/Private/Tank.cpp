92392f66c7d133484d7f205186c66d90204e6ef6
Quintin Oliver <quint.oliver@gmail.com>
2018-06-28 12:51:35 -0400

    Made projectiles move when fired and collide with tanks Video 156: Projectile Movement Components

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -45,14 +45,14 @@ void ATank::SetTurretReference(UTankTurret* TurretToSet) {
 }
 
 void ATank::Fire() {
-	UE_LOG(LogTemp, Warning, TEXT("Tank %s fired"), *GetName());
-
 	if (!Barrel) { return; }
 
 	// Spawn a projectile at the socket location of the barrel
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
 	);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
