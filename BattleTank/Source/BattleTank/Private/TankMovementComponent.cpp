d9890c11bd223f09a54bbf2e8ff68f7ca1e9a340
Quintin Oliver <quint.oliver@gmail.com>
2018-07-03 15:23:24 -0400

    Finished off the TankMovementComponent class Video 171: Finalizing Your Class Code

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Quintin Oliver
 
 #include "BattleTank.h"
 #include "TankTrack.h"
@@ -20,7 +20,6 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 
 	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
 	IntendTurnRight(RightThrow);
-	//UE_LOG(LogTemp, Warning, TEXT("%s right throw: %f"), *GetOwner()->GetName(), RightThrow)
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
