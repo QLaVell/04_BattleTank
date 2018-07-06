986f872ea184eb99ad86c0b566e6ed871f6171ac
Quintin Oliver <quint.oliver@gmail.com>
2018-07-06 08:58:42 -0400

    Moved aiming functionality from the Tank to the AimingComponent Video 183: Aiming Without the Tank

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
-#include "Public/Tank.h"
+#include "TankAimingComponent.h"
 #include "TankAIController.h"
 
 void ATankAIController::BeginPlay() {
@@ -12,16 +12,17 @@ void ATankAIController::BeginPlay() {
 void ATankAIController::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
-	auto PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
-	auto ControlledTank = Cast<ATank>(GetPawn());
-	if (ensure(PlayerTank)) {
-		// Move towards player
-		MoveToActor(PlayerTank, AcceptanceRadius); // TODO Check radius is in cm
+	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+	auto ControlledTank = GetPawn();
+	if (!ensure(PlayerController && ControlledTank)) { return; }
+	// Move towards player
+	MoveToActor(PlayerTank, AcceptanceRadius); // TODO Check radius is in cm
 
-		// Aim towards the player
-		ControlledTank->AimAt(PlayerTank->GetActorLocation());
+	auto AimingComponent = ControlledTank->FindComponentByClass<UTankAimingComponent>();
+	if (!ensure(AimingComponent)) { return; }
+	// Aim towards the player
+	AimingComponent->AimAt(PlayerTank->GetActorLocation());
 
-		ControlledTank->Fire(); // TODO Limit firing rate
-	}
-	
+	// TODO Fix firing
+	//ControlledTank->Fire();	
 }
