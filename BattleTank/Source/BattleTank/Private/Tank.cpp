1b459ce889c81bfa4d01c2e2e969386c9459840e
Quintin Oliver <quint.oliver@gmail.com>
2018-07-19 17:17:26 -0400

    Set up health bars for tanks Video 202: BlueprintPure & Health Bars

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -21,3 +21,7 @@ float ATank::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEve
 	}
 	return DamageToApply;
 }
+
+float ATank::GetHealthPercent() const {
+	return (float)CurrentHealth / (float)StartingHealth;
+}
