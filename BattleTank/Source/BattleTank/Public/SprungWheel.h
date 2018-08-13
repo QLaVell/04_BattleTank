b7a05d4329803587629077dbe025fc797b99862f
Quintin Oliver <quint.oliver@gmail.com>
2018-08-13 13:13:01 -0400

    Created class to handle tank shocks and tested it.

---
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -0,0 +1,33 @@
+// Copyright Quintin Oliver
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SprungWheel.generated.h"
+
+UCLASS()
+class BATTLETANK_API ASprungWheel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASprungWheel();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* Mass = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* Wheel = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UPhysicsConstraintComponent* MassWheelConstraint = nullptr;
+};
